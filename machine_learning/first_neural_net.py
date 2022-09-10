a863be4a1bcce6de72d8396ecb23c594a0f98c8e
kurt <kurt.stewart@bionic-power.com>
2022-09-09 21:45:03 -0700

    Change perceptron to step activation function

--- machine_learning/first_neural_net.py
+++ machine_learning/first_neural_net.py
@@ -1,14 +1,17 @@
 from tensorflow.keras import models, layers, utils, backend as K
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import shap
 
+def binary_step_activation(x):
+    return K.switch(x>0,tf.math.divide(x,x), tf.math.multiply(x,0))
 
 model = models.Sequential(name="Perceptron", layers=[
     layers.Dense(
         name = "dense",
         input_dim = 3,
         units = 1,
-        activation = 'linear'
+        activation = binary_step_activation #'linear'
     )
 ])
 
